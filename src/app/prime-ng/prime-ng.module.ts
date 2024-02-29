32f695cb3de4818c89174dfdaa489bed2b7259c4
Elvis Almenar <elvisalmenar@gmail.com>
2024-02-29 17:03:35 -0300

    Primer Commit

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
+
+
+@NgModule({
+  imports:[
+
+  ],
+  exports:[
+    MenubarModule,
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    PanelModule
+  ]
+})
+export class PrimeNgModule { }
